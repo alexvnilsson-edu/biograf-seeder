a52d414051ef283b53da3b933f274c3f69cffdaa
alexvnilsson <alexvnilsson@outlook.com>
2020-08-31 17:39:04 +0200

    Uppdatera cli-bibl.

--- build_tools/cli.sh
+++ build_tools/cli.sh
@@ -1,12 +1,48 @@
+DEBUG=1
+
 # Nyttiga variabler.
 python=python3
 setuptools=setup.py
+
+# Logiska variabler.
+arg_command=null
+command_func=null
 
 # Standardvärden.
 default_venv_path=.venv
 
 # Interna flaggor.
 venv_active=null
+
+#
+# Skriv ut ett meddelande endast under debugging (inklusive call stack).
+#
+function debucho() {
+    _caller=$(caller 1)
+    [[ $DEBUG = 1 ]] && echo "[${_caller}] $*"
+}
+
+function cli_make_error() {
+    _arg_badUsage=${1:-}
+
+    if [[ ! -z "${_arg_badUsage}" ]]; then
+        $_arg_badUsage
+    fi
+}
+
+function cli_set_command() {
+    _arg_command=$1
+    badUsage=${2:-}
+    debucho "setting command = ${_arg_command}"
+
+    if [[ -z "${_arg_command}" ]]; then
+        badUsage
+        exit 1
+    fi
+
+    arg_command=$_arg_command
+    command_func=app-$arg_command
+}
 
 function check_venv() {
     venv_active=$($python -c 'import os,sys; print(1 if os.getenv("VIRTUAL_ENV") is not None else 0)')
@@ -18,7 +54,7 @@ function try_venv_activate() {
     if [[ -d "${venv_path}" ]]; then
         source $venv_path/bin/activate
     else
-        echo "Hittade ingen virtualenv."
+        debucho "Hittade ingen virtualenv."
     fi
 }
 
@@ -27,9 +63,9 @@ function check_venv_active() {
 
     if [[ $venv_active != 1 ]]; then
         if [ "$arg_retry" != "no-retry" ]; then
-            (try_venv_activate && echo "OK" && check_venv && check_venv_active "no-retry") || (echo "Fel" && exit 1)
+            (try_venv_activate && debucho "OK" && check_venv && check_venv_active "no-retry") || (debucho "Fel" && exit 1)
         else
-            echo "Skippar återförsök pågrund av möjlig oändlig loop."
+            debucho "Skippar återförsök pågrund av möjlig oändlig loop."
             exit 1
         fi
         exit 0
@@ -37,5 +73,5 @@ function check_venv_active() {
 }
 
 function run_python_script() {
-    [[ $venv_active != 1 ]] && (echo "python3 without venv" && $python -m $*) || (echo "python with venv" && python -m $*)
+    [[ $venv_active != 1 ]] && (debucho "python3 without venv" && $python -m $*) || (debucho "python with venv" && python -m $*)
 }
