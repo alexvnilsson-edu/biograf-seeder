f8b8263b58d64f660b73a0e8d55c0eaf10981210
alexvnilsson <alexvnilsson@outlook.com>
2020-09-01 23:44:14 +0200

    build_tools: Uppdatera package.py.

--- build_tools/package.py
+++ build_tools/package.py
@@ -1,15 +1,76 @@
 #!/usr/bin/env python
 
 import click
-from subprocess import run
+import sys
+import os
+from os import path, getcwd
+import shutil
+from subprocess import run, PIPE
 from argparse import ArgumentParser
 from semver import SEMVER
 from process import Process
+
+python = "python3"
+
+
+def git_check_tracked(path: str) -> bool:
+    p = Process("git", "ls-files", "--error-unmatch", path, stdout=PIPE, stderr=PIPE)
+
+    try:
+        p.run()
+        return True
+    except Exception:
+        return False
 
 
 @click.group(name="package")
 def package():
     pass
+
+
+@package.command(name="clean")
+@click.argument("paths", nargs=-1, type=click.Path(file_okay=True, dir_okay=True))
+def clean(paths):
+    cwd = getcwd()
+
+    for d in paths:
+        dp = path.join(cwd, d)
+
+        if not git_check_tracked(dp):
+            if path.exists(dp):
+                if path.isdir(dp):
+                    shutil.rmtree(path.join(cwd, d))
+                elif path.isfile(dp):
+                    os.remove(path.join(cwd, dp))
+        else:
+            print(
+                f"Objektet {d} verkar tillhöra Git-indexet. Skippar osäker borttagning."
+            )
+
+
+@package.command(name="build")
+@click.option("--verbose", is_flag=True, default=False)
+def build(verbose: bool):
+    p = Process(python, "setup.py", "sdist")
+
+    if verbose:
+        p.add_argument("--verbose")
+
+    p.run()
+
+
+@package.command(name="publish")
+@click.option("--repository", type=click.STRING, default="testpypi")
+@click.argument("dist", type=click.Path(resolve_path=False), required=True)
+def publish(repository: str, dist: str):
+    p = Process("twine", "upload")
+
+    p.add_argument(f"--repository {repository}")
+    p.add_argument(dist)
+
+    print(p.cmd)
+
+    p.run()
 
 
 @package.group(name="version")
